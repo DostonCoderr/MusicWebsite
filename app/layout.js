ae758286005ba781e99c6bd1be6e68226a980652
Doston1232696 <otkurovdoston69@gmail.com>
2024-09-30 21:44:41 +0500

    add analitics in layout

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NavContextProvider from "./context/NavContext";
+import { Analytics } from "@vercel/analytics/react"
 
 const alex_Brush = Alex_Brush({
   weight: ["400"],
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
           <Header />
           {children}
           <Footer />
+          <Analytics/>
         </body>
       </html>
     </NavContextProvider>
